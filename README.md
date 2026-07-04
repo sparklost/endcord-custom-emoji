68040eefee8f9195ce54f84f40b9893413ce6742
sparklost <marko.b.zivic@gmail.com>
2026-07-04 17:33:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# endcord-custom-emoji
+An extension for [endcord](https://github.com/sparklost/endcord) discord TUI client, that adds command to insert any discord emoji or sticker as image url, so it will appear as embed.  
+Adds command `insert_custom_emoji [emoji/sticker]`. it expects `<:emoji_name:en=moji_id>` or `<;sticker_id;>` as argument.  
+They can be inserted with emoji/sticker assist.  
+Assist is overriden to show all emoji and stickers only for this command.
+
+## Installing
+See [official extensions documentation](https://github.com/sparklost/endcord/blob/main/extensions.md#installing-extensions) for installing instructions.
+Available options:
+- Git clone into `Extensions` directory located in endcord config directory.
+- Run `endcord -i https://github.com/sparklost/endcord-custom-emoji`
+- Or use endcord client-side command `install_extension sparklost/endcord-custom-emoji`
+
+## Configuration
+All extension options are under `[main]` section in endcord config. This extension options are always prefixed with `ext_custom_emoji_`.  
+
+### Settings options
+- `ext_custom_emoji_size = 32`  
+    Image size for sent emoji.
+- `ext_custom_sticker_size = 160`  
+    Image size for sent stickers.
+
+
+## Disclaimer
+> [!WARNING]
+> Using third-party client is against Discord's Terms of Service and may cause your account to be banned!  
+> **Use endcord and/or this extension at your own risk!**  
+> If this extension is modified, it may be used for harmful or unintended purposes.  
+> **The developer is not responsible for any misuse or for actions taken by users.**  
---
+++ endcord-custom-emoji.py
@@ -0,0 +1,67 @@
+import logging
+import re
+
+from endcord import formatter
+
+EXT_NAME = "Custom Emoji"
+EXT_VERSION = "0.1.0"
+EXT_ENDCORD_VERSION = "1.5.0"
+EXT_DESCRIPTION = "An extension that adds command to insert any discord emoji or sticker as image url, so it will appear as embed"
+EXT_SOURCE = "https://github.com/sparklost/endcord-custom-emoji"
+EXT_COMMAND_ASSIST = (
+    ("insert_custom_emoji - insert discord emoji as image url in input line", "insert_custom_emoji"),
+)
+logger = logging.getLogger(__name__)
+
+VS17 = "\U000E0100"
+EMOJI_SIZES = [32, 48, 56, 64, 96, 128, 160, 256, 512]
+STICKER_SIZES = [32, 64, 128, 160, 256, 512]
+
+
+class Extension:
+    """Main extension class"""
+
+    def __init__(self, app):
+        self.app = app
+        try:
+            self.emoji_size = int(app.config.get("ext_custom_emoji_size", 32))
+            self.emoji_size = min(EMOJI_SIZES, key=lambda x: abs(x - self.emoji_size))
+        except ValueError:
+            self.emoji_size = 32
+        try:
+            self.sticker_size = int(app.config.get("ext_custom_sticker_size", 160))
+            self.sticker_size = min(STICKER_SIZES, key=lambda x: abs(x - self.sticker_size))
+        except ValueError:
+            self.sticker_size = 32
+        self.app.premium_override_commands.append("insert_custom_emoji")
+        self.run = True
+
+
+    def on_execute_command(self, command_text, chat_sel, tree_sel):   # noqa
+        """Handle commands"""
+        if command_text.startswith("insert_custom_emoji"):
+            emoji_string = command_text[20:].strip()
+            match = re.match(formatter.match_d_emoji, emoji_string)
+            if match:
+                emoji_id = match.group(3)
+                if not emoji_id or not emoji_id.isdigit():
+                    self.app.update_extra_line("Invalid emoji", color=19)
+                    return True
+                url = f"https://{self.app.discord.cdn_host}/emojis/{emoji_id}.png?size={self.emoji_size}&quality=lossless&1"
+                self.app.insert_into_input_store(f"[{VS17}]({url})")
+                return True
+
+            match = re.match(formatter.match_sticker_id, emoji_string)
+            if match:
+                sticker_id = match.group()
+                if not sticker_id or not sticker_id[2:-2].isdigit():
+                    self.app.update_extra_line("Invalid sticker", color=19)
+                    return True
+                sticker_id = sticker_id[2:-2]
+                url = f"https://{self.app.discord.dyn_cdn_host}/stickers/{sticker_id}.png?size={self.sticker_size}"
+                self.app.insert_into_input_store(f"[{VS17}]({url})")
+                return True
+
+            self.app.update_extra_line("Invalid emoji", color=19)
+            return True
+        return False
